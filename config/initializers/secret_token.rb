7665115ced63fd7775412b1f6e862f47a2bccd80
Raj.BM <rajubassaragi@gmail.com>
2015-03-01 13:38:41 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Larks::Application.config.secret_key_base = '32298ddd20bc5b6924b4c3ca92a5849a83b8c94254d5591d907f8d163df8c1f6166b43f18085eff4d79d6853598b5267de1e1d048cc0932d11d6cb194e5dac00'
